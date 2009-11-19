c42d0bff218bdbcd8ee5796a5ac9da1781dde381
Marc S. Brooks <mbrooks@cpan.org>
2009-11-19 06:33:59 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-Session-Memcached-0.02 from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-Session-Memcached
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-Session-Memcached-0.02.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- lib/Apache2/WebApp/Plugin/Session/Memcached.pm
+++ lib/Apache2/WebApp/Plugin/Session/Memcached.pm
@@ -21,7 +21,7 @@ use Apache::Session::Memcached;
 use Apache::Session::Store::Memcached;
 use Params::Validate qw( :all );
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
@@ -77,24 +77,29 @@ sub create {
 
     untie %session;
 
+    $c->plugin('Cookie')->set( $c, {
+        name    => $name,
+        value   => $id,
+        expires => '24h',
+      });
+
     return $id;
 }
 
 #----------------------------------------------------------------------------+
-# get( \%controller, $name, $id )
+# get( \%controller, $name )
 #
 # Return session data as a hash reference.
 
 sub get {
-    my ( $self, $c, $name, $id )
+    my ( $self, $c, $name )
       = validate_pos( @_,
           { type => OBJECT  },
           { type => HASHREF },
-          { type => SCALAR  },
-          { type => SCALAR, optional => 1 }
+          { type => SCALAR  }
           );
 
-    my $cookie = $c->plugin('Cookie')->get($name) || $id;
+    my $cookie = $c->plugin('Cookie')->get($name);
 
     my $session_id = ($cookie) ? $cookie : "null";
 
